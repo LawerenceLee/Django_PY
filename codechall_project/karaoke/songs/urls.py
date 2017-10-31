f0545c2fdc617ff6a23a13453ab571f49d2e1125
Lawerence Lee <lucifitz.edward@gmail.com>
2017-10-30 23:52:35 -0700

    added empty lines for pep 8

--- codechall_project/karaoke/songs/urls.py
+++ codechall_project/karaoke/songs/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
     url(r'performer/(?P<pk>\d+)/$', views.performer_detail, name='performer'),
     url(r'song/(?P<pk>\d+)/$', views.song_detail, name='detail'),
